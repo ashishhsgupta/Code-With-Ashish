e31148a6526cb707742ed04cfdb3d6729f367f55
code with Ashish <ashishhsgupta11@gmail.com>
2023-01-14 22:23:44 +0530

    js

--- Calculate.js
+++ Calculate.js
@@ -1,20 +1,20 @@
-let screen=document.getElementById('out');
-buttons=document.querySelectorAll('button');
-let screenValue='';
-for(item of buttons){
-    item.addEventListener('click',(e)=>{
-        buttonText=e.target.innerText);
-        if(buttonText=='*'){
-            buttonText='*';
-            screenValue+=buttonText;
-            screen.value=screenValue;
-        }
-        else if(buttonText=='a/c'){
-            screenValue="";
-            screen.value=screenValue;
-        }
-           else if(buttonText=='='){
-            screenValue+=buttonText;
-            screen.value=screenvalue;
-           }
-        }
+let outputScreen=document.getElementById("output-screen");
+function display(num){
+    outputScreen.value+=num;
+}
+function calculate(){
+    try{
+        outputScreen.value=eval(outputScreen.value);
+    }
+    catch(err)
+    {
+
+alert("Invalid")
+}
+}
+function Clear(){
+    outputScreen.value = "";
+}
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
